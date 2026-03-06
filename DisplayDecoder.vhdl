bfb82040d1ecf5412f64d2eb77f533f77ba2e01a
jarek3660-cmd <jarek3660@gmail.com>
2026-03-05 17:32:23 -0700

    Update DisplayDecoder.vhdl

--- DisplayDecoder.vhdl
+++ DisplayDecoder.vhdl
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 03/05/2026 05:13:38 PM
+-- Create Date: 03/05/2026 05:02:45 PM
 -- Design Name: 
--- Module Name: DisplayDecoder_tb - Behavioral
+-- Module Name: DisplayDecoder - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,16 +31,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity DisplayDecoder_tb is
---  Port ( );
-end DisplayDecoder_tb;
-Port( Mux_out : in STD_LOGIC;
-     C_7SD, A_7SD : out STD_LOGIC_VECTOR (7 downto 0));
-architecture Behavioral of DisplayDecoder_tb is
-component DisplayDecoder
+entity DisplayDecoder is
+    Port ( DISP_in : in STD_LOGIC;
+           C_7SD, A_7SD : out STD_LOGIC_VECTOR (7 downto 0));
+end DisplayDecoder;
 
-end component;
+architecture Behavioral of DisplayDecoder is
 begin
+process(DISP_in)
+begin
+    if DISP_in = '1' then  
+        C_7SD <= "10010011"; 
+    else
+        C_7SD <= "11100011"; 
+    end if; 
+end process;  
 
-
+A_7SD <= "11111110";
 end Behavioral;
